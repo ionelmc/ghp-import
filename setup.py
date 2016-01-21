9733a1da2c04be7c3b6c44f48e023fbd844f90e8
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-01-21 05:03:15 +0200

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='ghp-import2',
-    version='1.0.0',
+    version='1.0.1',
     license='TPL',
     description='A GitHub Pages import tool.',
     long_description='%s\n%s' % (
